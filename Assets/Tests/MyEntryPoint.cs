3aaed739c1333be0a4981cf6216cc61bd1002ecc
Aleksei Antipin <alxj.ant@gmai.com>
2024-05-24 21:11:17 +0400

    documentation

---
+++ Assets/Tests/MyEntryPoint.cs
@@ -0,0 +1,14 @@
+﻿using Abyss.StartupManager;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "MyEntryPoint", menuName = "Startup Manager/MyEntryPoint", order = 1)]
+public class MyEntryPoint : EntryPointScriptableObject
+{
+	#region Overrides
+	public override void ResolveObjectGraph()
+	{
+		var service = new InitializableService1();
+		AddInitializable(service);
+	}
+	#endregion
+}
